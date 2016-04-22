2fa0ed0ff6d2353c68cc7d414ad84102ef0a767c
leo81202 <nucklesix@gmail.com>
2016-04-21 20:45:57 -0400

    updated index.js some ES6 stuff

--- js/index.js
+++ js/index.js
@@ -1,54 +1,53 @@
-"use strict"
+"use strict";
+//"esversion: 6";
 
-var quoteApi = "https://andruxnet-random-famous-quotes.p.mashape.com/"
-var quoteText;
-var quoteAuthor;
-var colorSelectArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
-var color = "#";
 
-var requestObj = {
+let quoteApi = 'https://andruxnet-random-famous-quotes.p.mashape.com/';
+let requestObj = {
   url: quoteApi, // Where to retrive the quote
   type: "Get",
   beforeSend: function(xhr) { // Sending Headers needed for the api request
     xhr.setRequestHeader("X-Mashape-Key", "GLrO1Wam6rmsh36WREqRoCBbpTvKp1U2u46jsn1LhbmJOEy7Zp");
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.setRequestHeader("Accept", "application/json")
+    xhr.setRequestHeader("Accept", "application/json");
   },
   success: DisplayQuote,
   dataType: "json", // data type of the retrived file
-
-}
+};
 
 $(document).ready(function() { // runs when the page is loaded
-  GetColor();
+  let color;
+  color = GetColor(color);
   GetQuote();
-  
+
   $("body").css("background-color", color);
 
 });
 $("#new_quote").click(function() { // Gets new Quote on click.
-  color = "#";
+  let color;
+   color = GetColor(color);
   GetQuote();
-  GetColor();
   $("body").css("background-color", color);
 
 });
 
 function DisplayQuote(quoteObj) { // Displays the quote.
-  quoteText = quoteObj.quote;
-  quoteAuthor = quoteObj.author;
+  let quoteText = quoteObj.quote;
+  let quoteAuthor = quoteObj.author;
   $("#quote").html(quoteText).addClass("h3");
   $("#author").html(quoteAuthor);
   $("#tweet_btn").attr("href", "https://twitter.com/intent/tweet?text=" + quoteText + "%0a--- " + quoteAuthor);
 }
 
-function GetColor() { // Generates a Random Color
-
-  var num;
-  for (i = 0; i <= 5; i++) {
+function GetColor(color) { // Generates a Random hex Color
+  color = "#";
+  const colorSelectArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+  let num;
+  for (let i = 0; i <= 5; i++) {
     num = Math.floor(Math.random() * colorSelectArray.length);
     color += colorSelectArray[num];
   }
+  return color;
 }
 
 function GetQuote() {
